943641b01e90ca5968d8ca567552f6781b28fe2e
shah-paradox <[shahmeeramircoder@gmail.com]>
2025-07-04 16:25:18 +0500

    feat: Gemini api works

---
+++ test.py
@@ -0,0 +1,17 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+topic = input('Kindly enter a topic. Include any sub-details if reuqired\n')
+
+
+client = genai.Client()
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Context: Generate a list of Reddit channels that related to the following topic:"+topic,
+)
+
+
+print(response.text)
